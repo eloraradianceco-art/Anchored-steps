fe5b9acdde7eb7121a5be1edd8ec21a541f44ca8
eloraradianceco-art <eloraradiance.co@gmail.com>
2026-04-10 12:45:33 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AnchoredSteps from './App.jsx'
+import '../public/data.js'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AnchoredSteps />
+  </React.StrictMode>
+)
