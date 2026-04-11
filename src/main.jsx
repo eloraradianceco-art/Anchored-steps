3f498820456e6e999eca45e849ff10f906cee13a
eloraradianceco-art <eloraradiance.co@gmail.com>
2026-04-11 18:16:19 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AnchoredSteps from './App.jsx'
+import ResetPassword from './ResetPassword.jsx'
+
+const isReset = window.location.pathname === '/reset-password'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AnchoredSteps />
+    {isReset ? <ResetPassword /> : <AnchoredSteps />}
   </React.StrictMode>
 )
